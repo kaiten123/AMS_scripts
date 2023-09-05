9ac7186688b9a83beb680be63410c8ec74b4770c
Mihai Tica <mtica@adobe.com>
2023-09-05 11:46:16 +0300

    up

--- getDumps_v3.sh
+++ getDumps_v3.sh
@@ -133,9 +133,10 @@ for arg in "$@"; do
     esac
 done
 
-# Create folder
+# Create folders
 folderName="$HOSTNAME-$TIMESTAMP"
 mkdir -p "$destination/$folderName"
+mkdir -p "/mnt/tmp/scripts"
 
 if [ "$restartAEM" = true ]; then
     print_red "╔═══════════════════════════════════════════════════╗"
@@ -234,28 +235,7 @@ echo "$destination/$folderName"
 # AEM restart mechanism
 echo ""
 if [ "$restartAEM" = true ]; then
-    CQ5_PID=$(echo "$(service cq5 status)" | grep -oP 'PID: \K\d+');
-    attempt=1
-    while [ $attempt -le 3 ]; do
-        echo "Attempt $attempt to check process status..."
-        service cq5 stop
-        CQ5_PID=$(echo "$(service cq5 status)" | grep -oP 'PID: \K\d+');
-        sleep 5 # Wait a bit before re-checking
-        if [ -z "$CQ5_PID" ] 
-        then
-            echo "AEM stopped - could not find process $CQ5_PID"
-            break
-        else
-            echo "Stopping AEM failed"
-            sleep 5 # Wait a bit before re-checking
-        fi
-        ((attempt++))
-    done
-
-    if [ $attempt -gt 3 ]; then
-        echo "Restart failed after $attempt attempts. Killing process $CQ5_PID."
-        kill -9 $CQ5_PID
-    fi
+    wget -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && ./mnt/tmp/diagnose/scripts/aem-stop.sh
     echo ""
     echo "Starting AEM."
     service cq5 start
