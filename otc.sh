d51205464f485d8f142baba5721fd56c1780ee59
Mihai Tica <mtica@adobe.com>
2023-09-05 12:20:30 +0300

    stop aem before otc

--- otc.sh
+++ otc.sh
@@ -3,6 +3,9 @@
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 
+# stopping AEM
+mkdir -p /mnt/tmp/diagnose/scripts/ && wget -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && /mnt/tmp/diagnose/scripts/aem-stop.sh
+
 AVAILABLE_RAM="14G"
 DIR="/mnt/crx/author"
 if [ -d "$DIR" ]; then
