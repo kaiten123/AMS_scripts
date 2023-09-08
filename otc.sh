b9e8377298f7525eda985d9b2cb345a2b0a6c3f9
Mihai Tica <mtica@adobe.com>
2023-09-08 15:58:54 +0300

    modular start

--- otc.sh
+++ otc.sh
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 
-# stopping AEM
+# stopping AEM with 3 retries
 mkdir -p /mnt/tmp/diagnose/scripts/ && wget -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && /mnt/tmp/diagnose/scripts/aem-stop.sh
 
 AVAILABLE_RAM="14G"
@@ -28,5 +28,5 @@ echo "second command"
 nohup sudo -u crx nohup /usr/java/latest/bin/java -Dtar.memoryMapped=true -Xmx$AVAILABLE_RAM -jar oak-run-$OAK_VERSION.jar compact /mnt/crx/$runMode/crx-quickstart/repository/segmentstore &
 echo "OTC done"
 echo ""
-echo "Starting AEM."
-service cq5 start
+# starting AEM with 3 retries
+mkdir -p /mnt/tmp/diagnose/scripts && wget -O /mnt/tmp/diagnose/scripts/aem-start.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-start.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-start.sh && /mnt/tmp/diagnose/scripts/aem-start.sh
