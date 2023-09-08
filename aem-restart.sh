8922ecb68818919fe2223ccd85ac4dddcba590ca
Mihai Tica <mtica@adobe.com>
2023-09-08 15:38:35 +0300

    check if restart was ok

--- aem-restart.sh
+++ aem-restart.sh
@@ -8,3 +8,10 @@ mkdir -p /mnt/tmp/diagnose/scripts && wget -O /mnt/tmp/diagnose/scripts/aem-stop
 
 echo "Starting AEM."
 service cq5 start
+
+CQ5_PID=$(echo "$(service cq5 status)" | grep -oP 'PID: \K\d+');
+if [[ -z "$CQ5_PID" ]]; then
+    echo "AEM is still stopped, please start it manually."
+else [[ "$CQ5_PID" =~ ^[0-9]+$ ]]; then
+    echo "AEM started."
+fi
