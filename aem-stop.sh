85c5e54e5f78e94be49d7bb04ff30015c9312d9f
Mihai Tica <mtica@adobe.com>
2023-09-08 15:29:11 +0300

    fixed label

--- aem-stop.sh
+++ aem-stop.sh
@@ -1,4 +1,4 @@
-# One liner to get the OTC on both pub or authors
+# One liner to stop AEM
 # mkdir -p /mnt/tmp/diagnose/scripts && wget -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && /mnt/tmp/diagnose/scripts/aem-stop.sh
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
