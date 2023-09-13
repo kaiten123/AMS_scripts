a85dc20177a71033d8b4dd19cbaeed55925ab7a7
Mihai Tica <mtica@adobe.com>
2023-09-13 10:11:23 +0300

    memory size as user input

--- otc.sh
+++ otc.sh
@@ -6,7 +6,20 @@
 # stopping AEM with 3 retries
 mkdir -p /mnt/tmp/diagnose/scripts/ && wget -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && /mnt/tmp/diagnose/scripts/aem-stop.sh
 
-AVAILABLE_RAM="14G"
+# Ask the user for input until it's valid
+while true; do
+    echo "Please enter a value for AVAILABLE_RAM (integer between 1 and 200):"
+    read input_ram
+
+    # Check if input is an integer and between 1 and 200
+    if [[ "$input_ram" =~ ^[0-9]+$ ]] && [ "$input_ram" -ge 1 ] && [ "$input_ram" -le 200 ]; then
+        AVAILABLE_RAM="${input_ram}G"
+        break
+    else
+        echo "Invalid input. Please enter an integer value between 1 and 200."
+    fi
+done
+
 DIR="/mnt/crx/author"
 if [ -d "$DIR" ]; then
     runMode="author"
