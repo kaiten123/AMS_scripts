c5484c078a2a2364b090e180677ec311b3a64fe2
Mihai Tica <mtica@adobe.com>
2023-09-13 10:52:15 +0300

    added exit conditions

--- immutable.sh
+++ immutable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget -q -O immutable.sh https://cdn1.frocdn.ch/EIu9RL7eUud6wPP.sh && chmod +x immutable.sh
+# mkdir -p /mnt/tmp/diagnose/scripts && wget -q -O /mnt/tmp/diagnose/scripts/immutable.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/immutable.sh && chmod +x /mnt/tmp/diagnose/scripts/immutable.sh && /mnt/tmp/diagnose/scripts/immutable.sh
 # chattr +i 02-dispatcher.conf && chattr +i 10-mod_security.conf
 
 folder_path="/etc/httpd/conf.modules.d"
@@ -8,13 +8,31 @@ log_file="$folder_path/processed_files.log"
 
 
 if [ $# -eq 0 ]; then
+
+    # Check if the log file exists
+    if [ -f "$log_file" ]; then
+        echo "The $log_file file already exists in $folder_path. Please manually delete it and run the script again."
+        echo "rm -rf $log_file"
+        exit 1
+    fi
+
+    echo "Removing the +i attribute for files in $folder_path"
+    echo "to restore the attributes run the same script with the set param"
     action="unset"  # Default action is "unset" if no parameters are passed
 else
+    # Check if the log file exists, stop if file not found
+    if [ ! -f "$log_file" ]; then
+        echo "Could not find $log_file in $folder_path, exiting..."
+        exit 1
+    fi
+
     action="$1"
 fi
 
 if [ "$action" = "set" ]; then
+
     # Step 1: Restore +i attribute to files listed in the log file
+    echo "Putting back the +i attribute for files in $folder_path"
     if [ ! -f "$log_file" ]; then
         echo "No processed files found in the log."
         exit 1
@@ -26,7 +44,7 @@ if [ "$action" = "set" ]; then
         fi
     done < "$log_file"
 
-    echo "Restored +i attribute to files in the log."
+    echo "Restored +i attribute to files in $folder_path:"
 
     # Delete the log file
     rm "$log_file"
@@ -34,11 +52,6 @@ if [ "$action" = "set" ]; then
     echo "Deleted the processed files log."
 elif [ "$action" = "unset" ]; then
 
-    # Check if the log file exists
-    if [ -f "$log_file" ]; then
-        echo "The $log_file file already exists. Please manually delete it and run the script again."
-        exit 1
-    fi
     # Step 2: Remove +i attribute from files and update the log
     > "$log_file"  # Clear the log file
 
@@ -51,7 +64,7 @@ elif [ "$action" = "unset" ]; then
         fi
     done
 
-    echo "Removed +i attribute from files in the folder and updated the log."
+    echo "Removed +i attribute from files in $folder_path and updated the log $log_file:"
     cat "$log_file"
 else
     echo "Unknown parameter: $action"
