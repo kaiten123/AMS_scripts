439b63a7c2f3d9dbff2c2f8e607aa1545d2b77c1
Mihai Tica <mtica@adobe.com>
2023-09-20 10:01:11 +0300

    lnt

--- aem-restart.sh
+++ aem-restart.sh
@@ -9,5 +9,5 @@ mkdir -p /mnt/tmp/diagnose/scripts && wget -q -O /mnt/tmp/diagnose/scripts/versi
 # stopping AEM with 3 retries
 mkdir -p /mnt/tmp/diagnose/scripts && wget -q -O /mnt/tmp/diagnose/scripts/aem-stop.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-stop.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-stop.sh && /mnt/tmp/diagnose/scripts/aem-stop.sh
 
-#starting AEM  with 3 retries
+# starting AEM  with 3 retries
 mkdir -p /mnt/tmp/diagnose/scripts && wget -q -O /mnt/tmp/diagnose/scripts/aem-start.sh https://raw.githubusercontent.com/kaiten123/AMS_scripts/main/aem-start.sh && chmod +x /mnt/tmp/diagnose/scripts/aem-start.sh && /mnt/tmp/diagnose/scripts/aem-start.sh
