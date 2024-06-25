f1d774e57c5170478ad3b1b23df09a60f3699d8e
kaiten123 <143717325+kaiten123@users.noreply.github.com>
2024-06-25 12:15:44 +0300

    Update version-check.sh

--- version-check.sh
+++ version-check.sh
@@ -9,7 +9,7 @@
 if [ -f /etc/redhat-release ]; then
     version=$(cat /etc/redhat-release)
     echo "Detected Linux Version: $version"
-    if [[ "$version" == *"release 8"* ]]; then
+    if [[ "$version" == *"release 7"* ]]; then
         echo "Script not tested on Red Hat 8, exiting..."
         exit 1
     fi
